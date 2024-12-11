d19783a511b1a2b1a1379cd8769ec1778dd793b7
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-11 17:00:07 +0530

    Initial commit - project setup & user signup extracting & validating use input

---
+++ components/auth-form.js
@@ -0,0 +1,39 @@
+'use client';
+import { signUp } from '@/actions/auth-actions';
+import Link from 'next/link';
+import { useFormState } from 'react-dom';
+
+export default function AuthForm() {
+
+  const [formState,formAction] =useFormState(signUp,{})
+
+  return (
+    <form id="auth-form" action={formAction}>
+      <div>
+        <img src="/images/auth-icon.jpg" alt="A lock icon" />
+      </div>
+      <p>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+      </p>
+      <p>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </p>
+
+      { formState.errors && (<ul id='form-errors'>
+          {Object.keys(formState.errors).map((x)=>(
+            <li key={x}>{formState.errors[x]}</li>
+          ))}
+         </ul>)}
+      <p>
+        <button type="submit">
+          Create Account
+        </button>
+      </p>
+      <p>
+        <Link href="/">Login with existing account.</Link>
+      </p>
+    </form>
+  );
+}
